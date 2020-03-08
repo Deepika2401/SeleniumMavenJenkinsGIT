e6dc9dad3cc5f7646bc6933bd49339828af44a92
Deepika2401 <24deepikagautam@gmail.com>
2020-03-08 13:17:41 +0530

    commiting code from CMD

---
+++ FirstMavenProject/src/test/java/samplePackage/SecondMavenClass.java
@@ -0,0 +1,13 @@
+package samplePackage;
+
+import org.testng.annotations.Test;
+
+public class SecondMavenClass {
+	
+	@Test
+	public void test2()
+	{
+		System.out.println("Second test2");
+	}
+
+}
---
+++ FirstMavenProject/src/test/java/samplePackage/ThirdMavenClass.java
@@ -0,0 +1,13 @@
+package samplePackage;
+
+import org.testng.annotations.Test;
+
+public class ThirdMavenClass {
+	
+	@Test
+	public void test3()
+	{
+		System.out.println("Test3");
+	}
+
+}
